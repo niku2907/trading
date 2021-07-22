11c74f65dcd7fac23ea4ec31c86e3699df81525d
niku2907 <36333850+niku2907@users.noreply.github.com>
2021-07-23 00:58:41 +0530

    Fetch historical NIFTYBANK data

---
+++ BankNifty/data_reader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 22 17:06:52 2021
+
+@author: nishant.gupta
+"""
+
+
+# This class reads the data in the xls file and creates a pandas stream out of
+# it.
+
+import pandas as pd
+
+class data_reader:
+    def read(file_name, sheet_name):
+        return pd.read_excel(file_name, sheet_name)
+    
+#exp1 = data_reader.read("Historical_data.xlsx", "June21")
+#exp2 = data_reader.read("Historical_data.xlsx", "July21")
