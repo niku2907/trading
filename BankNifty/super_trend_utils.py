f70a51637b1ae6ee4f66a80179ac5baee8058de6
niku2907 <36333850+niku2907@users.noreply.github.com>
2021-09-20 22:33:44 +0530

    Using Super trend indicator for NIFTY stocks

--- BankNifty/super_trend_utils.py
+++ BankNifty/super_trend_utils.py
@@ -113,21 +113,67 @@ class super_trend_utils:
         return False
     
     @staticmethod
+    def can_buy_based_on_st(prev_st, prev_price, current_st, current_price):
+        if (prev_st > prev_price and current_st < current_price):
+            return True
+        return False
+    
+    @staticmethod
+    def can_sell_based_on_st(prev_st, prev_price, current_st, current_price):
+        if (prev_st < prev_price and current_st > current_price):
+            return True
+        return False
+    
+    @staticmethod
     def get_pnl(buy_price, sell_price):
         if (buy_price > sell_price):
             print("Loss of: ", buy_price - sell_price)
         else:
             print("Profit of: ", sell_price - buy_price)
         
-        return sell_price - buy_price
+        return super_trend_utils.get_pnl_including_taxes(buy_price, sell_price, 1, 100)
     
+    @staticmethod
+    def print_debug_log(message, level):
+        if (level > 0):
+            print("[DEBUG] ", message)
+        
+    @staticmethod
+    def get_pnl_including_taxes(buy_price, sell_price, num_lots, lot_size):
+        brokerage = 40
+        transaction_charges = (.00345/100) * (buy_price + sell_price) * num_lots * lot_size
+        stt = (.025/100) * sell_price * num_lots * lot_size
+        stamp_duty = (.003/100) * buy_price * num_lots * lot_size
+        sebi_charges = (10/100) * stamp_duty
+        gst = (18/100) * (brokerage + transaction_charges)
+        total_charges = brokerage + transaction_charges + stt + stamp_duty + sebi_charges + gst
+        pnl = (sell_price - buy_price) * num_lots * lot_size - total_charges
+        return pnl
+    
+    @staticmethod
+    def update_transaction_record(pos_buy_price, buy_time, pos_sell_price, sell_time,\
+                                  buy_price_list, buy_time_list, sell_price_list, sell_time_list,\
+                                  pos_type, pos_type_list, current_pnl, pnl_list,\
+                                  exit_method, exit_method_list):
+        buy_price_list.append(pos_buy_price)
+        sell_price_list.append(pos_sell_price)
+        buy_time_list.append(buy_time)
+        sell_time_list.append(sell_time)
+        pos_type_list.append(pos_type)
+        pnl_list.append(current_pnl)
+        exit_method_list.append(exit_method)
+        
     # SUPERTREND STRATEGY
     @staticmethod
-    def implement_st_strategy(data, have_cut_off_time_check):
+    def implement_st_strategy(data, have_cut_off_time_check, ema_period, target_pct,\
+                              sl_pct):
         prices = data['Close']
         st = data['st']
         date = data['Date']
-        ema = data['EMA50']
+        ema = data['EMA' + str(ema_period)]
+        high_prices = data['High']
+        low_prices = data['Low']
+        open_prices = data['Open']
         
         cut_off_time_to_start = '14:30:00'
         cut_off_time_to_close = '15:00:00'
@@ -138,6 +184,16 @@ class super_trend_utils:
         pos_buy_price = 0
         pos_sell_price = 0
         total_pnl = 0
+        sl_price = 0
+        target_price = 0
+
+        buy_time_list = []
+        sell_time_list = []
+        buy_price_list = []
+        sell_price_list = []
+        position_type_list = []
+        pnl_list = []
+        exit_method_list = []
         
         status_list = ['NA' for i in range(len(st))]
         signal_list = ['NA' for i in range(len(st))]
@@ -150,84 +206,191 @@ class super_trend_utils:
                 continue
             
             current_time = util.get_time(date[i])
+            
+            # Check if autosqaure off needed.
             if (have_cut_off_time_check == 1):
                 if (current_time >= cut_off_time_to_close):
                     if (is_open_pos == 1):
+                        pos_type = "BUY"
+                        exit_method = "Cutoff BUY"
                         if (open_pos_type == "BUY"):
-                            pos_buy_price = prices[i]
-                            print("Autosquare off initiated. Buy price: ", pos_buy_price)
-                            open_pos_type = ""
-                            is_open_pos = 0
-                            status_list[i] = "BUY"
-                        else:
                             pos_sell_price = prices[i]
                             print("Autosquare off initiated. Sell price: ", pos_sell_price)
                             open_pos_type = ""
                             is_open_pos = 0
-                            status_list[i] = "SELL"
+                            status_list[i] = "[Cutoff] SELL"
+                            sell_time = date[i]
+                            exit_method = "Cutoff SELL"
+                        else:
+                            pos_buy_price = prices[i]
+                            print("Autosquare off initiated. Buy price: ", pos_buy_price)
+                            open_pos_type = ""
+                            is_open_pos = 0
+                            status_list[i] = "[Cutoff] BUY"
+                            buy_time = date[i]
+                            pos_type = "SELL"
                         
-                        total_pnl += super_trend_utils.get_pnl(pos_buy_price, pos_sell_price)
+                        current_pnl = super_trend_utils.get_pnl(pos_buy_price, pos_sell_price)
+                        super_trend_utils.update_transaction_record(pos_buy_price, buy_time,\
+                                            pos_sell_price, sell_time,\
+                                            buy_price_list, buy_time_list,\
+                                            sell_price_list, sell_time_list,\
+                                            pos_type, position_type_list, current_pnl, pnl_list,\
+                                            exit_method, exit_method_list)
+                        total_pnl += current_pnl
                     continue
     
                 if (current_time > cut_off_time_to_start):
-                    print("Not trying to initate a new trade after cutoff time.")
+                    message = "Not trying to initate a new trade after cutoff time."
+                    super_trend_utils.print_debug_log(message, 0)
                     continue
             
+            if (is_open_pos == 1):
+                if (open_pos_type == "BUY"):
+                    can_close_pos = 0
+                    exit_method = ""
+                    # Check if SL hit.
+                    if (low_prices[i] <= sl_price):
+                        pos_sell_price = low_prices[i]
+                        print("[SL] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[SL] SELL"
+                        exit_method = "SL SELL"
+                    # Check if target achieved.
+                    elif (high_prices[i] >= target_price):
+                        pos_sell_price = target_price
+                        print("[Target] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[Target] SELL"
+                        exit_method = "Target SELL"
+                    # Check if ST signal changed.
+                    elif (super_trend_utils.can_sell_based_on_st(st[i-1], prices[i-1], st[i], prices[i])):
+                        pos_sell_price = prices[i]
+                        print("[ST] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[ST] SELL"
+                        exit_method = "ST SELL"
+
+                    if (can_close_pos == 1):
+                        sell_time = date[i]
+                        is_open_pos = 0
+                        open_pos_type = "NA"
+                        current_pnl = super_trend_utils.get_pnl(pos_buy_price,\
+                                                               pos_sell_price)
+                        total_pnl += current_pnl
+                        super_trend_utils.update_transaction_record(pos_buy_price, buy_time,\
+                                            pos_sell_price, sell_time,\
+                                            buy_price_list, buy_time_list,\
+                                            sell_price_list, sell_time_list,\
+                                            "BUY", position_type_list, current_pnl, pnl_list,\
+                                            exit_method, exit_method_list)
+                elif (open_pos_type == "SELL"):
+                    can_close_pos = 0
+                    exit_method = ""
+                    # Check if SL hit.
+                    if (high_prices[i] >= sl_price):
+                        pos_buy_price = high_prices[i]
+                        print("[SL] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[SL] BUY"
+                        exit_method = "SL BUY"
+                    elif (low_prices[i] <= target_price):
+                        pos_buy_price = target_price
+                        print("[Target] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[Target] BUY"
+                        exit_method = "Target BUY"
+                    # Check if ST signal changed.
+                    elif (super_trend_utils.can_buy_based_on_st(st[i-1], prices[i-1], st[i], prices[i])):
+                        pos_buy_price = prices[i]
+                        print("[ST] Closing position. Time: ", date[i],\
+                              " Buy Price: ", pos_buy_price, " Sell Price: ",\
+                                  pos_sell_price)
+                        can_close_pos = 1
+                        status_list[i] = "[ST] BUY"
+                        exit_method = "ST BUY"
+                        
+                    if (can_close_pos == 1):
+                        buy_time = date[i]
+                        is_open_pos = 0
+                        open_pos_type = "NA"
+                        current_pnl = super_trend_utils.get_pnl(pos_buy_price,\
+                                                               pos_sell_price)
+                        total_pnl += current_pnl
+                        super_trend_utils.update_transaction_record(pos_buy_price, buy_time,\
+                                            pos_sell_price, sell_time,\
+                                            buy_price_list, buy_time_list,\
+                                            sell_price_list, sell_time_list,\
+                                            "SELL", position_type_list, current_pnl, pnl_list,\
+                                            exit_method, exit_method_list)
+                continue
+            
+            # Check if we can open a new position.                                     
             if (super_trend_utils.can_buy(st[i-1], prices[i-1], st[i], prices[i],\
                                           ema[i])) :
                 signal_list[i] = "BUY"
+                potential_sl = prices[i] - st[i]
+                allowed_sl = prices[i] * sl_pct / 100
+                if (potential_sl > allowed_sl):
+                    message = "SL needed is not in the limits."
+                    super_trend_utils.print_debug_log(message, 0)
+                    continue
+                target_price = prices[i] * (1 + target_pct / 100)
+                buy_time = date[i]
                 
                 # Ideally sell price should be opening price of the next candle.
-                if (is_open_pos == 0):
-                    buy_time = util.get_time(date[i])
-                    pos_buy_price = prices[i]
-                    print("Buy trade initiated at: ", buy_time, " Price: ",\
-                          pos_buy_price)
-                    is_open_pos = 1
-                    open_pos_type = "BUY"
-                    status_list[i] = "BUY"
-                else:
-                    if (open_pos_type == "BUY"):
-                        print("Already an open BUY position for the same day.")
-                    elif (open_pos_type == "SELL"):
-                        buy_time = util.get_time(date[i])
-                        pos_buy_price = prices[i]
-                        print("Closing position. Time: ", buy_time, "Buy price: ",\
-                              pos_buy_price, " Sell price: ", pos_sell_price)
-                        is_open_pos = 0
-                        open_pos_type = "NA"
-                        status_list[i] = "BUY"
-                        total_pnl += super_trend_utils.get_pnl(pos_buy_price,\
-                                                               pos_sell_price)
-                        
+                pos_buy_price = prices[i]
+                print("Buy trade initiated at: ", date[i], " Price: ",\
+                      pos_buy_price)
+                is_open_pos = 1
+                open_pos_type = "BUY"
+                status_list[i] = "BUY"
+                sl_price = st[i]
             elif (super_trend_utils.can_sell(st[i-1], prices[i-1], st[i], prices[i],\
                                              ema[i])):
                 signal_list[i] = "SELL"
+                potential_sl =  st[i] - prices[i]
+                allowed_sl = prices[i] * sl_pct / 100
+                if (potential_sl > allowed_sl):
+                    message = "SL needed is not in the limits."
+                    super_trend_utils.print_debug_log(message, 0)
+                    continue
+                target_price = prices[i] * (1 - target_pct / 100)
+                sell_time = date[i]
                 
                 # Ideally sell price should be opening price of the next candle.
-                if (is_open_pos == 0):
-                    sell_time = util.get_time(date[i])
-                    pos_sell_price = prices[i]
-                    print("Sell trade initiated at: ", sell_time, " Price: ",\
-                          pos_sell_price)
-                    is_open_pos = 1
-                    open_pos_type = "SELL"
-                    status_list[i] = "SELL"
-                else:
-                    if (open_pos_type == "SELL"):
-                        print("Already an open SELL position for the same day.")
-                    elif (open_pos_type == "BUY"):
-                        sell_time = util.get_time(date[i])
-                        pos_sell_price = prices[i]
-                        print("Closing position. Time: ", sell_time, "Sell price: ",\
-                              pos_sell_price, " Buy price: ", pos_buy_price)
-                        is_open_pos = 0
-                        open_pos_type = "NA"
-                        status_list[i] = "SELL"
-                        total_pnl += super_trend_utils.get_pnl(pos_buy_price,\
-                                                               pos_sell_price)
+                pos_sell_price = prices[i]
+                print("Sell trade initiated at: ", date[i], " Price: ",\
+                      pos_sell_price)
+                is_open_pos = 1
+                open_pos_type = "SELL"
+                status_list[i] = "SELL"
+                sl_price = st[i]
+            else:
+                # dynamic SL.
+                if (is_open_pos == 5):
+                    if (open_pos_type == "BUY"):
+                        if (sl_price < st[i] and st[i] < pos_buy_price):
+                            print("Trailing the SL for BUY position.")
+                            sl_price = st[i]
+                    elif (open_pos_type == "SELL"):
+                        if (sl_price > st[i] and st[i] > pos_sell_price):
+                            print("Trailing the SL for SELL position.")
+                            sl_price = st[i]
+                
                             
-        return total_pnl, status_list, signal_list
+        return total_pnl, status_list, signal_list, position_type_list, buy_time_list, buy_price_list,\
+            sell_time_list, sell_price_list, pnl_list, exit_method_list
 
                 
     # Plot super trend signals. 
---
+++ BankNifty/zerodha_ticker_id.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 20:57:44 2021
+
+@author: nishant.gupta
+"""
+
+name_zerodha_nse_id_realty_dict = dict({"3887105":"BRIGADE",\
+                                        "3771393":"DLF",\
+                                        "4576001":"GODREJPROP",\
+                                        "179457":"HEMIPROP",\
+                                        "3699201":"IBREALEST",\
+                                        "5181953":"OBEROIRLTY",\
+                                        "3725313":"PHOENIXLTD",\
+                                        "5197313":"PRESTIGE",\
+                                        "3539457":"SOBHA",\
+                                        "4516097":"SUNTECK"})
+
+name_zerodha_nse_id_it_dict = dict({"2955009":"COFORGE",\
+                                    "4561409":"LTI",\
+                                    "3675137":"MINDTREE",\
+                                    "1152769":"MPHASIS",\
+                                    "2748929":"OFSS"})
+
+name_zerodha_nse_id_fmcg_dict = dict({"3876097":"COLPAL",\
+                                      "197633":"DABUR",\
+                                      "3460353":"EMAMI",\
+                                      "2585345":"GODREJCP",\
+                                      "4632577":"JUBLFOODS",\
+                                      "1041153":"MARICO",\
+                                      "648961":"PGHH",\
+                                      "4278529":"UBL",\
+                                      "2674433":"MCDOWELL",\
+                                      "4843777":"VBL"})
+
+
+
+name_zerodha_nse_id_dict = dict({"3861249":"ADANIPORT",\
+                                 "60417":"ASIANPAINTS",\
+                                 "1510401":"AXISBANK",\
+                                 "4267265":"BAJAJ-AUTO",\
+                                 "81153":"BAJFINANCE",\
+                                 "4268801":"BAJAJFINSV",\
+                                 "134657":"BPCL",\
+                                 "2714625":"BHARTI-AIRTEL",\
+                                 "140033":"BRITANIA",\
+                                 "177665":"CIPLA",\
+                                 "5215745":"COALINDIA",\
+                                 "2800641":"DIVISLAB",\
+                                 "225537":"DRREDDY",\
+                                 "232961":"EICHERMOT",\
+                                 "315393":"GRASIM",\
+                                 "1850625":"HCLTECH",\
+                                 "341249":"HDFCBANK",\
+                                 "119553":"HDFCLIFE",\
+                                 "345089":"HEROMOTOCO",\
+                                 "348929":"HINDALCO",\
+                                 "356865":"HUL",\
+                                 "340481":"HDFC",\
+                                 "1270529":"ICICI",\
+                                 "424961":"ITC",\
+                                 "415745":"IOC",\
+                                 "1346049":"INDUSIND",\
+                                 "408065":"INFY",\
+                                 "3001089":"JSWSTEEL",\
+                                 "492033":"KOTAKBANK",\
+                                 "2939649":"LT",\
+                                 "519937":"M&M",\
+                                 "2815745":"MARUTI",\
+                                 "2977281":"NTPC",\
+                                 "4598529":"NESTLE",\
+                                 "633601":"ONGC",\
+                                 "3834113":"POWERGRID",\
+                                 "738561":"RELIANCE",\
+                                 "5582849":"SBILIFE",\
+                                 "794369":"SHREECEM",\
+                                 "779521":"SBIN",\
+                                 "857857":"SUNPHARMA",\
+                                 "2953217":"TCS",\
+                                 "878593":"TATACONSUM",\
+                                 "884737":"TATAMOTORS",\
+                                 "895745":"TATASTEEL",\
+                                 "3465729":"TECHM",\
+                                 "897537":"TITAN",\
+                                 "2889473":"UPL",\
+                                 "2952193":"ULTRACEM",\
+                                 "969473":"WIPRO",\
+                                 "3887105":"BRIGADE",\
+                                 "3771393":"DLF",\
+                                 "4576001":"GODREJPROP",\
+                                 "179457":"HEMIPROP",\
+                                 "3699201":"IBREALEST",\
+                                 "5181953":"OBEROIRLTY",\
+                                 "3725313":"PHOENIXLTD",\
+                                 "5197313":"PRESTIGE",\
+                                 "3539457":"SOBHA",\
+                                 "4516097":"SUNTECK",\
+                                 "2955009":"COFORGE",\
+                                 "4561409":"LTI",\
+                                 "3675137":"MINDTREE",\
+                                 "1152769":"MPHASIS",\
+                                 "2748929":"OFSS",\
+                                 "3876097":"COLPAL",\
+                                 "197633":"DABUR",\
+                                 "3460353":"EMAMI",\
+                                 "2585345":"GODREJCP",\
+                                 "4632577":"JUBLFOODS",\
+                                 "1041153":"MARICO",\
+                                 "648961":"PGHH",\
+                                 "4278529":"UBL",\
+                                 "2674433":"MCDOWELL",\
+                                 "4843777":"VBL"})
